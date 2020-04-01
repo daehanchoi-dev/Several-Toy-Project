1f749ab4e7d3bff1dde5dc331893bcf7822009e5
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-04-01 21:41:19 +0900

    cat & dog
    Cats and Dogs Classification Using little data

---
+++ Cat_Dog.py
@@ -0,0 +1,83 @@
+from keras_preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as b
+
+# dimensions of images
+img_width, img_height = 150, 150
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+train_samples = 2000
+validation_samples = 800
+epochs = 50
+batch_size = 16
+
+if b.image_data_format() == 'Cat_Dog_Classification':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3,3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(32, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer = 'rmsprop',
+              metrics=['accuracy'])
+
+# Data augmentaion for increasing accuracy
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255) # only rescaling on test_data
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=train_samples // batch_size,
+    epochs= epochs,
+    validation_data = validation_generator,
+    validation_steps= validation_samples // batch_size
+)
+
+model.save_weights('Cats_and_Dogs_Classification.h5')
