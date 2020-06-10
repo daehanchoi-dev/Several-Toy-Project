6810f58e57d97e8a2c1dae72f7a0d3338d1cef5d
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-06-10 19:04:01 +0900

    Update ImageClassification.py

--- ImageClassification.py
+++ ImageClassification.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
+
 # Load Data
 
 from keras.datasets import cifar10
@@ -26,7 +27,7 @@ print(x_train[0])
 print('\ntab\n')
 print(y_train[0])
 """
-
+# 인덱스 순서... 인덱스 0 번 부터 비행기, 자동차, 새, 고양이 ..... 9번은 트럭
 classification = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 # 레이블을 원핫 인코딩으로 숫자로 치환해주기
@@ -101,3 +102,42 @@ plt.xlabel('epoch')
 plt.legend(['Train', 'Val'], loc='upper right')
 plt.savefig('Loss.png')
 plt.show()
+
+# 예제를 통한 모델 테스트
+import cv2
+
+Image = cv2.imread('cat.jpg')
+cv2.imshow('Cute_cat', Image)
+cv2.waitKey()
+
+# 이미지를 입력하기 위해 크기 조정
+
+from skimage.transform import resize
+
+resized_image = resize(Image, (32,32,3))
+cv2.imshow('Cute_Cat', resized_image)
+
+
+# 모델을 통한 예측
+
+prediction = model.predict(np.array([resized_image]))
+print(prediction)
+
+# 이미지의 유사도가 큰 순서대로 인덱스 정렬하기
+
+index = [0,1,2,3,4,5,6,7,8,9]
+x = prediction
+
+for i in range(10):
+    for j in range(10):
+        if x[0][index[i]] > x[0][index[j]]:
+            idx = index[i]
+            index[i] = index[j]
+            index[j] = idx
+
+print(index)
+
+# 가장 유사한 5개의 인덱스
+
+for i in range(5):
+    print(classification[index[i]], ':', round(prediction[0][index[i]] * 100, 2), '%')
